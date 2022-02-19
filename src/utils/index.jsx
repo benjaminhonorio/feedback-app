24c424bba4d5b3488e917ecf6056d2e106cb4670
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-02-18 23:15:14 -0500

    Generalize capitalize function for composite strings

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,1 +1,10 @@
-export const capitalize = (text) => text.charAt(0).toUpperCase() + text.slice(1)
+export const capitalize = (text) => {
+  const words = text.split(' ')
+  if (words.length > 1) {
+    return words.map(t => t.charAt(0).toUpperCase() + t.slice(1)).reduce((newText, t) => {
+      return `${newText} ${t}`
+    }, '').trim()
+  } else {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  }
+}
